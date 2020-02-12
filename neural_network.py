12e2891b5713d5031f1450567c869855cff218fa
alessandroarezzo <alessandro.arezzo@stud.unifi.it>
2020-02-12 11:53:18 +0100

    Replaced function for calculating the distance between two points in the neural network with the calculation of the norm (for points in multiple dimensions)

--- neural_network.py
+++ neural_network.py
@@ -1,12 +1,14 @@
 import numpy as np
 import math
 import csv
-def calculateDistance(x1,y1,x2,y2):
-     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-     return dist
+from mpl_toolkits import mplot3d
+from PendulumDynamics import PendulumDynamics
+from matplotlib import pyplot as plt
+
 
 def rbf(x, c, s):
-    value=np.exp(-1 / (2 * s**2) * (calculateDistance(x[0],x[1],c[0],c[1]))**2)
+    dist = np.linalg.norm(x - c)
+    value = np.exp(-1 / (2 * s ** 2) * (dist) ** 2)
     return value
 
 # Class implements the neural network for approximate the Q function in ER algorithm
@@ -17,19 +19,6 @@ class RBFNet(object):
         self.lr = lr # learning rate
         self.rbf = rbf
         self.centers=centers
-        """
-        try:
-            parameters=np.load( pathFileParameters,allow_pickle=True )
-            self.w={}
-            for a in actions:
-                self.w[a]=parameters.item(0).get(a)
-                
-        except FileNotFoundError:
-            self.w={}
-            for a in actions:
-                self.w[a] = np.zeros(k)
-        """
-
         try:
             self.w = {}
             for a in actions:
@@ -49,19 +38,57 @@ class RBFNet(object):
         if standard_deviations:
             self.standard_deviations=standard_deviations
         else:
-            dMax = max([np.abs(calculateDistance(c1[0],c2[0],c1[1],c2[1])) for c1 in self.centers for c2 in self.centers])
+            dMax = max([np.abs(np.linalg.norm(c1 - c2)) for c1 in self.centers for c2 in self.centers])
             self.standard_deviations = np.repeat(dMax / np.sqrt(2 * self.k), self.k)
 
 
     def fit(self, X, y, u):
         a = np.array([self.rbf(X, c, s) for c, s, in zip(self.centers, self.standard_deviations)])
         F = a.T.dot(self.w.get(u))
-        error = -(y - F).flatten()
-        self.w[u] = self.w.get(u) - self.lr * a * error
+        error = (y - F).flatten()
+        self.w[u] = self.w.get(u) + self.lr * a * error
 
     def predict(self, X, u):
         a = np.array([self.rbf(X, c, s) for c, s, in zip(self.centers, self.standard_deviations)])
         y_pred = a.T.dot(self.w.get(u))
         return y_pred
 
+#Neural network testing
 
+if __name__ == '__main__':
+    dynamics=PendulumDynamics()
+    grid=dynamics.generateRBFGrid(11)
+    neural_net=RBFNet(len(grid),0.1,grid,dynamics.actions,"./data/weights_testNet.csv")
+    n_samples=1000
+    X_SAMPLES_THETA=[]
+    X_SAMPLES_THETADOT=[]
+    X_SAMPLES=[]
+    Y_SAMPLES=[]
+
+    for i in range(0,n_samples):
+        randomState=dynamics.casualState()
+        X_SAMPLES_THETA.append(randomState[0])
+        X_SAMPLES_THETADOT.append(randomState[1])
+        Y_SAMPLES.append(randomState[0]+randomState[1]*2)
+        X_SAMPLES.append(randomState)
+        for j in range(0,10):
+            neural_net.fit([X_SAMPLES_THETA[i],X_SAMPLES_THETADOT[i]],Y_SAMPLES[i],0)
+
+    ax = plt.axes(projection='3d')
+    ax.plot3D(X_SAMPLES_THETA, X_SAMPLES_THETADOT, Y_SAMPLES, 'gray')
+    plt.show()
+
+    X_THETA=[]
+    X_THETADOT=[]
+    X=[]
+    y=[]
+    for i in range(0,n_samples):
+        randomState=dynamics.casualState()
+        X_THETA.append(randomState[0])
+        X_THETADOT.append(randomState[1])
+        X.append(randomState)
+        y.append(neural_net.predict(randomState,0))
+
+    ax = plt.axes(projection='3d')
+    ax.plot3D(X_THETA, X_THETADOT, y, 'gray')
+    plt.show()
